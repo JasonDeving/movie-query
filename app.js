7cc892020d072bf559757aed4b3d4aa6e2cb8100
jchan031 <aznj50@hotmail.com>
2016-05-30 04:36:51 +0000

    movie title search app

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+app.set("view engine", "ejs")
+
+app.get("/", function(req, res) {
+    res.render("search");
+})
+
+app.get("/results", function(req, res){
+    var query = req.query.search;
+    request("http://www.omdbapi.com/?s=" + query, function(error, response, body){
+       if (!error && response.statusCode ===200){
+           var data = JSON.parse(body);
+           //res.send(results["Search"][0]["title"]);
+           res.render("results", {data: data});
+       } 
+    });
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log('server has started localhost:3000');
+});
